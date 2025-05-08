caa84d7d30d4d281bd16069baeeab6495eadceb4
Matthew Kwon <matthew.o.kwon@gmail.com>
2025-05-07 22:15:57 -0700

    Initialized Spring Boot project

---
+++ FairShare/src/main/java/com/mkwon/FairShare/FairShareApplication.java
@@ -0,0 +1,13 @@
+package com.mkwon.FairShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairShareApplication.class, args);
+	}
+
+}
---
+++ FairShare/src/test/java/com/mkwon/FairShare/FairShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkwon.FairShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
